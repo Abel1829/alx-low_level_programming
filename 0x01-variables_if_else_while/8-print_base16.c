ce864f15640a9e57e15c441168d41cc1de607778
Abel1829 <abelzewdie21@gmail.com>
2022-06-03 07:01:46 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'a'; b <= 'f'; b++);
+	{
+		putchar(b);
+	}
+	putchar('\n');
+}	
