ddcfb5fb8a530e9b83764bd8a6cc4411ff7c3122
Abel1829 <abelzewdie21@gmail.com>
2022-06-07 12:06:13 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,8 @@
 /**
  * _islower - Function that checks for lowercase character.
  *
+ *@c - The character is ASCII code.
+ *
  * Return: 1 for lowercase character. 0 for the rest.
  */
 int _islower(int c)
