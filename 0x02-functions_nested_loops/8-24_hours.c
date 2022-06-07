9d2bbc733c06a21c32c1ab4abdc71ff2422e5087
Abel1829 <abelzewdie21@gmail.com>
2022-06-07 14:23:40 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,24 +6,40 @@
  */
 void jack_bauer(void)
 {
-	int i, j, k, n;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-	for (i = 48; i <= 50; i++)
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
 	{
-		for (j = 48; j <= 52; j++)
+		if (hours_tens == '2')
 		{
-			for (k = 48; k <= 53; k++)
-			{
-				for (n = 48; n <= 57; n++)
-				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(58);
-					_putchar(k);
-					_putchar(n);
-					_putchar('\n');
-				}
-			}
+			hours_max = '4';
 		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
 	}
 }
