172cff233778d830852858a21637bb0118dc660f
Abel1829 <abelzewdie21@gmail.com>
2022-06-09 07:05:38 -0700

    corrections

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,21 +6,22 @@
  */
 void more_numbers(void)
 {
-	char a, b;
+	char n, c;
 	int i = 0;
 
 	while (i < 10)
 	{
-		for (a = 0; a <= 14; a++)
+		for (n = 0; n <= 14; n++)
 		{
-			b = a;
-			if (a > 9)
+			c = n;
+			if (n > 9)
 			{
 				_putchar('1');
-				b = a % 10;
+				c = n % 10;
 			}
-			_putchar('0' + a);
+			_putchar('0' + c);
 		}
+
 		_putchar('\n');
 		i++;
 	}
