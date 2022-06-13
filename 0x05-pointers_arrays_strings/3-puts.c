bf1fc1c9f47cc8da18855581fbd22b4c48d59fa9
Abel1829 <abelzewdie21@gmail.com>
2022-06-13 01:10:01 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - A function that prints a string.
+ *
+ * @str: a pointer that points to char.
+ *
+ * Return: string.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i[str]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
