975ae6f5f5fe42a0733bccb676f075b2777e4692
Abel1829 <abelzewdie21@gmail.com>
2022-06-13 05:46:12 -0700

    every character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - A function that prints every character of a string.
+ *
+ * @str: A pointer that points to char.
+ *
+ * Return: void.
+ */
+void puts2(char *str)
+{
+        int i;
+
+        for (i = 0; str[i] != '\0'; i++)
+        {
+                if (i % 2 == 0)
+                {
+                        _putchar(str[i]);
+                }
+        }
+        _putchar('\n');
+}
