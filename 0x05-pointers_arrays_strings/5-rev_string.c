50f9dafd9703860ca9dc7f721c0fb58b9410eb64
Abel1829 <abelzewdie21@gmail.com>
2022-06-13 05:14:39 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - A function that returns a string.
+ *
+ * @s: a pointer points to char.
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, len, tmp;
+
+	len = strlen(s);
+	for (i = 0; i < len/2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len-i-1];
+		s[len-i-1] = tmp;
+	}
+}
