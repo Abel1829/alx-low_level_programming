041b26c195202a25122570a5f0d307a80383de85
Abel1829 <abelzewdie21@gmail.com>
2022-06-13 00:26:51 -0700

    98 battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * rest_to_98 - A function that takes a pointer to an int as parameter.
+ *
+ * @n: integer pointer.
+ *
+ * Return: *n.
+ */
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
