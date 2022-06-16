201edf6db5904f9f81e2e382f7e1d2f0475c0b41
Abel1829 <abelzewdie21@gmail.com>
2022-06-15 21:35:46 -0700

    string concatenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,8 +11,12 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0, len = 0;
 
 	while (dest[i++])
+	{
 		len++;
+	}
 	for (i = 0; src[i] && i < n; i++)
+	{
 		dest[len++] = src[i];
+	}
 	return (dest);
 }
