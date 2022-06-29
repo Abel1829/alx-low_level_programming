c870220bf5ae1c7c8a98f7a5417e6c27f3e105b1
Abel1829 <abelzewdie21@gmail.com>
2022-06-29 07:02:19 -0700

    correction

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - A function that checks the integer is digit.
+ *
+ * @c: Used as an integer.
+ *
+ * Return: 1 if it is digit else 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
