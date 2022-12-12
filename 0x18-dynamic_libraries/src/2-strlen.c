70b1513c08ff8ea235e3803e49ada6c8c214abd8
Abel1829 <abelzewdie21@gmail.com>
2022-12-12 08:29:00 -0800

    dynamic libraries

---
+++ 0x18-dynamic_libraries/src/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function to count a string length
+ * @s: parameter to be counted
+ * Return: returns the length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
